e22fe57372fdb10190a8847ae1aa704ba934694e
Jan Stolzenbach <j.stolzenbach@student.uni-tuebingen.de>
2016-04-17 11:26:34 +0200

    added Hello World for conventions sake

--- helloWorld/Main.scala
+++ helloWorld/Main.scala
@@ -5,12 +5,12 @@ import com.cra.figaro.algorithm.factored.VariableElimination
 object Main extends App {
   val morning = Flip(0.4)
   val greeting = If(morning,
-    Select(0.1 -> "Good Morning!",0.9 -> "Where is my coffee?"),
-    Select(0.5 -> "Hi there!", 0.5 -> "Where is my coffee?"))
+    Select(0.1 -> "Good Morning!",0.9 -> "Hello World! Where is my coffee?"),
+    Select(0.5 -> "Hi there!", 0.5 -> "Hello World! Where is my coffee?"))
 
   def predict(): Unit ={
-    val result = VariableElimination.probability(greeting, "Where is my coffee?")
-    println("Your greeting will be \"Where is my coffee?\" " +
+    val result = VariableElimination.probability(greeting, "Hello World! Where is my coffee?")
+    println("Your greeting will be \"Hello World! Where is my coffee?\" " +
     "with probability " + result + ".")
   }
   predict()
