2d6e723bd484879b7a8f21415e441139e0397a3d
Jan Stolzenbach <j.stolzenbach@student.uni-tuebingen.de>
2016-07-25 23:17:29 +0200

    added index generation

--- ShiftRecommender/src/main/scala/Model.scala
+++ ShiftRecommender/src/main/scala/Model.scala
@@ -1,8 +1,9 @@
 import java.time.LocalDate
 
 import com.cra.figaro.algorithm.sampling.Importance
-import com.cra.figaro.language.{Apply, Constant, Element, Flip}
+import com.cra.figaro.language._
 import com.cra.figaro.library.atomic.continuous.Beta
+import com.cra.figaro.library.atomic.discrete.Uniform
 import com.cra.figaro.library.collection.{Container, FixedSizeArray}
 
 /**
@@ -22,7 +23,7 @@ object Model {
   class Worker() {
     val taskQuality = new FixedSizeArray[Double](numTasks, _ => Beta(1, 1))
     // i d like defaultQuality.clone here
-    val timetable = new FixedSizeArray[Boolean](numDays * numShifts, (_: Int) => Flip(0.5)) // multidimensional fixedarrays are not supported
+    val timetable = new FixedSizeArray[Boolean](numDays * numShifts, (_: Int) => Flip(0.5))
 
     def hasTime(day: Int, shift: Int): Element[Boolean] = {
       timetable((day - 1) * numShifts + shift)
@@ -33,8 +34,22 @@ object Model {
     * Shift(workers) represents an assignment of possible workers to tasks
     * Attention: number of possible workers > number of tasks
     */
-  class Shift(workers: Array[Worker]) {
-    val taskQualifications = new FixedSizeArray[Double](numTasks, i => workers(i).taskQuality(i))
+  class Shift(workers: Seq[Worker]) {
+    val index = generateIndexWoDup(numTasks)
+    val taskQualifications = new FixedSizeArray[Double](numTasks, i => workers(index(i).value).taskQuality(i))
+
+    def generateIndexWoDup(num:Int):Seq[Element[Int]] = {
+      for (i <- 0 to num) yield
+        Chain(Uniform(0,numWorkers),chainFunction)
+    }
+
+    def chainFunction(num:Int): Element[Int] = {
+      val values: Seq[Int] = Seq.range(0,numWorkers-1)
+      var duplicates = List[Int]()
+
+      duplicates = num :: duplicates
+      Uniform(values.filterNot(duplicates.contains(_)): _*)
+    }
 
     def sumQuality(): Element[Double] = taskQualifications.foldLeft(0.0)(_ + _)
   }
@@ -46,16 +61,17 @@ object Model {
     */
 
   class Week(end: LocalDate) {
-    val workers = Array.fill(numWorkers)(new Worker())
-    val shifts = Array.fill(numShifts)(new Shift(workers))
+    val workers = Seq.fill(numWorkers)(new Worker())
+    val shifts = Seq.fill(numShifts*numDays)(new Shift(workers))
 
-    def sumQuality(): Element[Double]= Container(shifts.map(x => x.sumQuality()): _*).foldLeft(0.0)(_ + _)
+
+    def sumQuality(): Element[Double]= Container(shifts.map(_.sumQuality()): _*).foldLeft(0.0)(_ + _)
   }
 
   def main(args: Array[String]) {
     val week = new Week(LocalDate.now())
-    week.shifts(0).sumQuality().addCondition(_ > 2.0)
-    val alg = Importance(1000, week.shifts(0).taskQualifications.elements: _*)
+    week.sumQuality().addCondition(_ > 2.0)
+    val alg = Importance(10000, week.shifts(0).taskQualifications.elements: _*)
     alg.start()
     val test = alg.probability(week.shifts(0).taskQualifications(1), (x: Double) => x > 0.1)
     println(test)
