a975f78a5f3075f59b5370d726fb708b6bc50f72
Jan Stolzenbach <j.stolzenbach@student.uni-tuebingen.de>
2016-07-31 16:24:45 +0200

    fixed bug in generateUniqueIndex method, sadly this reintroduces performance issues

--- ShiftRecommender/src/main/scala/Model.scala
+++ ShiftRecommender/src/main/scala/Model.scala
@@ -51,26 +51,20 @@ object Model {
     * Shift(workers) represents an assignment of possible workers to tasks
     * Attention: number of possible workers > number of tasks
     */
-  class Shift(workers: Seq[Worker], day: Int, shift: Int) {
+  class Shift(workers: Seq[Worker], day: Int, shift: Int){
     var j = 0;
-    // val index = generateIndexWoDup(numTasks)
-    val sumQuality = qualifications.foldLeft(0)(_+_)
-    // new FixedSizeArray[Int](numTasks, i => workers(index(i).value).taskQuality(i))
+    lazy val index = generateUniqueIndex(numTasks)
+    //val qualifications = new FixedSizeArray[Int](numTasks, i => Chain(index, (ls:List[Int]) => workers(ls(i)).taskQuality(i)))
     val qualifications = new FixedSizeArray[Int](numTasks, i => workers(i).taskQuality(i))
-    //val qualifications = index.chain(i => chainQual(i))
+    val sumQuality = qualifications.foldLeft(0)(_+_)
 
-    def generateIndexWoDup(num: Int): FixedSizeArray[Int] = {
-      var duplicates = List[Int]()
-      def chainIndex(num: Int): Element[Int] = {
-        val values: Seq[Int] = 0 until numWorkers
-
-        duplicates = num :: duplicates
-        Uniform(values
-          .filterNot(duplicates.contains(_)) // ensures there are no duplicates
-          .filterNot(workers(_).hasTime(day, shift)) // ensures workers has time
-          : _*)
-      }
-      new FixedSizeArray[Int](num,(i:Int) => Chain(Uniform(0, numWorkers), chainIndex))
+    def generateUniqueIndex(num: Int):Element[List[Int]]={
+      val values = 0 until numWorkers
+      def uniqueCondition(seq:Seq[Int]) = seq.distinct.size == seq.size
+      val index = new FixedSizeArray[Int](num, i => Uniform(values: _*))
+      val allIndices : Element[List[Int]] = Inject(index.elements: _*)
+      allIndices.setCondition(uniqueCondition)
+      allIndices
     }
 
 
@@ -90,15 +84,12 @@ object Model {
     *
     */
 
-  class Week() {
-    val workers = Seq.tabulate(numWorkers)(i=> new Worker(i))
-    val shifts = Seq.tabulate(numDays, numShifts)((day, shift) => new Shift(workers, day, shift))
+  class Week(workers: Seq[Worker]) {
+    lazy val shifts = Seq.tabulate(numDays, numShifts)((day, shift) => new Shift(workers, day, shift))
+    lazy val sumQuality = getSumQuality()
 
 
-    /*def sumQuality(): Element[Double] = Container(shifts
-      .flatten // from sequence[][] to sequence[]
-      .map(_.sumQuality: _*) // create sequence[] of sums
-      .foldLeft(0.0)(_ + _) // create global sum of qualities of a week*/
+    def getSumQuality(): Element[Int] = Container(shifts.flatten.map(_.sumQuality): _*).foldLeft(0)(_+_)
   }
 
   def main(args: Array[String]) {
@@ -107,16 +98,13 @@ object Model {
     for(i<- 0 until trainSize) {
       val shift = new Shift(workers,0,0)
       shift.sumQuality.observe(100)
-      //shift.sumQuality.observe(0.6)
     }
     val futureShift = new Shift(workers,0,0)
     println(Universe.universe.activeElements.toString())
-    val alg = Importance(1000,futureShift.qualifications.elements: _*)
+    val alg = Importance(100,futureShift.qualifications.elements: _*)
     println("Starting sampling")
     alg.start()
     val test = alg.probability(futureShift.qualifications(0),(i:Int) => i < 10)
     println(test)
-    println(VariableElimination.probability(futureShift.qualifications(0),75))
-
   }
 }
